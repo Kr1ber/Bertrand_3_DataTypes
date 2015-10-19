0a7937874ed71c9d466ea556f01149d55007b66b
Bertrand Kristopher <kriber8678@gmail.com>
2015-10-19 05:57:02 -0500

    Primes

---
+++ src/bertrand_3_datatypes/Bertrand_3_DataTypes.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bertrand_3_datatypes;
+
+/**
+ *
+ * @author Kriber
+ */
+public class Bertrand_3_DataTypes {
+
+    public static void main(String[] args) {
+        //This is a comment
+        //We will use many comments to explain our code
+        //This program explores some of the primitive data types
+        int i = 0;
+        float a = 0.01f;
+        //f makes it recognize it as a float
+        double b = 0.01;
+        short c = 123;
+        char d = 123;
+        String e = "Words";
+        boolean f = true;
+        //First letter of "String" is CAPS
+        
+        
+        /*
+        Here we will look at some mathematical operators
+        */
+        // + this adds two numbers and more
+        // - this subtracts two numbers and more
+        // * this multiplies two numbers and more
+        // / this divides two numbers and more
+        // % this is modulo. Shows the remainder 
+        
+        
+        System.out.println(i + " an integer");
+        System.out.println(a + " a float");
+        System.out.println(b + " a double");
+        System.out.println(c + " a short");
+        System.out.println(d + " a char");
+        System.out.println(e + " a string");
+        System.out.println(f + " a boolean");
+        
+    }
+    
+}
